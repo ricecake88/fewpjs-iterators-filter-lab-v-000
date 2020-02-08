b2f5c503fc699e73ea89f2f0b5e2e56d78a32deb
Grace Shih <vocalise@gmail.com>
2020-02-08 22:42:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const findMatching = function(array_of_drivers, driver_name) {
 const fuzzyMatch = function(array_of_drivers, driver_name) {
   return array_of_drivers.filter(driver => { return driver[0].toUpperCase() === driver_name[0].toUpperCase(); })
 }
+
+const matchName = function(array_of_drivers, driver_name) {
+  return array_of_drivers.filter(driver => { return driver[0].toUpperCase() === driver_name[0].toUpperCase(); })
+}
